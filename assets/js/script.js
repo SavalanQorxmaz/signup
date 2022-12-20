9b54a50b6c4c3caedb2775a52cb7a06cb1a78e1b
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2022-12-20 13:48:16 +0400

    added script with span

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+  
+    $("input").click(function(){
+        $(this).prevAll().css({"opacity": "1"});
+    });
+    $("select").click(function(){
+        $(this).prevAll().css({"opacity": "1"});
+    });
+  
+
+//   $(document).ready(function(){
+   
+     
+//     });
+  
+//   });
+
+//   $(document).ready(function(){
+//     $("input").prev().css({"visibility": "hidden"});
+//     });
+  
+ 
